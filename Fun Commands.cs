ec8247b7d5cd8767e22eceef68dd8b96aabee2ea
Earth-Panda <62580742+Earth-Panda@users.noreply.github.com>
2021-04-16 22:07:17 -0300

    This is just stupid

--- Fun Commands.cs
+++ Fun Commands.cs
@@ -1,1 +1,23 @@
 // Fun commands
+
+/*
+Essensial
+FPS
+Single player (Weapon type: Guns/grenade/potions) 
+Building with blocks - (Build phase between waves/rounds) 
+point system for ie. monney for building/items
+Simple visuals (shaders/textures)
+Enemies in waves (enemy type can varry)
+Sounds (just basic sounds)
+Default backround city
+
+if time permits
+Player models
+multiplayer 
+multiple backrounds
+*/
+
+//print to the Debug window
+Debug.Log("hello World");
+
+//
--- New Unity Project/Assets/PlayerMovement.cs
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+
+    public float moveForce = 1000F;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Add a forward force
+        //rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(moveForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, moveForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-moveForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -moveForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, moveForce * Time.deltaTime, 0);
+        }
     }
 }
