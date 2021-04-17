9a842f8f348c1247467b4e7223eb300d3828d964
Earth-Panda <62580742+Earth-Panda@users.noreply.github.com>
2021-04-16 22:08:16 -0300

    Fun Commands updated

--- Fun Commands.cs
+++ Fun Commands.cs
@@ -1,7 +1,8 @@
 // Fun commands
 
+//THE BEST GAME EVER MADE tm
 /*
-Essensial
+Essensial:
 FPS
 Single player (Weapon type: Guns/grenade/potions) 
 Building with blocks - (Build phase between waves/rounds) 
@@ -11,13 +12,19 @@ Enemies in waves (enemy type can varry)
 Sounds (just basic sounds)
 Default backround city
 
-if time permits
+If time permits:
 Player models
 multiplayer 
 multiple backrounds
 */
 
 //print to the Debug window
-Debug.Log("hello World");
+Debug.Log("Hello World");
 
-//
+//Gravity and forces
+obj.useGravity = boolean;
+obj.AddFoce(x,y,z);
+
+//Values
+//this is supper important as it evens out between framrates
+Time.deltaTime
