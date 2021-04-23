8c90d494b4abd9030fa3213fc574cfb301eedee0
Earth-Panda <62580742+Earth-Panda@users.noreply.github.com>
2021-04-22 21:10:21 -0300

    :)

---
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public int EnemyHealth = 10;
+
+    void DeductPoints(int DamageAmount) {
+        EnemyHealth -= DamageAmount;
+    }
+
+    void Update() {
+        if (EnemyHealth <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
